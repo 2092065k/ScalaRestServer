729df6d05791b813b7ff0dd289eb2caa1c91c94a
Ivan <ivannnn1994@gmail.com>
2018-12-18 16:48:50 +0000

    Hello World rest service

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,6 +1,22 @@
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import spray.routing.SimpleRoutingApp
 
-object HelloWorld extends App {
+import scala.concurrent.duration._
 
-    println("Hello World!")
+object HelloWorld extends App with SimpleRoutingApp {
+
+    implicit val actorSystem = ActorSystem()
+    implicit val timeout = Timeout(1.second)
+
+    startServer(interface = "localhost", port = 8080) {
+        get {
+            path("") {
+                complete {
+                    "Hello World!"
+                }
+            }
+        }
+    }
 
 }
